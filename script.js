85f2b857586bf1b0bb85189daf3235c0721a9634
Prajwal Wani <119412705+Prajwalwani05@users.noreply.github.com>
2023-05-23 23:59:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-//your JS code here. If required.
-function alert(){
-	let n = document.getElementById('level').value;
-    alert("The Level of the Element is: " + n);
+// script.js
+var liElement = document.getElementById("level");
+var level = getLevel(liElement);
+
+alert("The level of the element is: " + level);
+
+// Function to get the level of an element
+function getLevel(element) {
+    var level = 0;
+    while (element.parentNode) {
+        element = element.parentNode;
+        if (element.nodeName === "UL") {
+            level++;
+        }
+    }
+    return level;
 }
-alert();
